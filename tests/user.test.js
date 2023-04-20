9451c7f669eca9b2def861861a528b6b276d2ef8
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-04-20 16:45:51 +0800

    init

---
+++ tests/user.test.js
@@ -0,0 +1,35 @@
+const request = require("supertest");
+
+const express = require("express");
+const app = express();
+
+//
+app.get("/users/", (req, res) => {
+  const users = [
+    {
+      id: 1,
+      name: "Jonh Doe",
+      email: "john@gmail.com",
+      id: 2,
+      name: "Ike Doe",
+      email: "ike@gmail.com",
+    },
+  ];
+  res.json(users);
+});
+describe("GET /users", () => {
+  it("responde with a list of users", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([
+      {
+        id: 1,
+        name: "John Doe",
+        email: "john@gmail.com",
+        id: 2,
+        name: "Ike Doe",
+        email: "ike@gmail.com",
+      },
+    ]);
+  });
+});
