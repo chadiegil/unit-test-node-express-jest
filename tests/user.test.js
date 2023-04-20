2f6525a73e5974d8f827ea3d1b291693597bcae1
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-04-20 17:13:09 +0800

    update

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,33 +3,49 @@ const request = require("supertest");
 const express = require("express");
 const app = express();
 
-//
-app.get("/users/", (req, res) => {
-  const users = [
-    {
-      id: 1,
-      name: "Jonh Doe",
-      email: "john@gmail.com",
-      id: 2,
-      name: "Ike Doe",
-      email: "ike@gmail.com",
-    },
-  ];
-  res.json(users);
+app.get("/sample", (req, res) => {
+  const answer = 1 * 1;
+
+  res.json(answer);
 });
-describe("GET /users", () => {
+
+describe("GET /sample", () => {
   it("responde with a list of users", async () => {
-    const response = await request(app).get("/users");
+    const expectedResponse = 1;
+    const response = await request(app).get("/sample");
     expect(response.status).toBe(200);
-    expect(response.body).toEqual([
-      {
-        id: 1,
-        name: "John Doe",
-        email: "john@gmail.com",
-        id: 2,
-        name: "Ike Doe",
-        email: "ike@gmail.com",
-      },
-    ]);
+    expect(response.body).toEqual(expectedResponse);
   });
 });
+
+//
+// app.get("/users", (req, res) => {
+//   const users = [
+//     {
+//       id: 1,
+//       name: "Jonh Doe",
+//       email: "john@gmail.com",
+//       id: 2,
+//       name: "Ike Doe",
+//       email: "ike@gmail.com",
+//     },
+//   ];
+//   res.json(users);
+// });
+
+// describe("GET /users", () => {
+//   it("responde with a list of users", async () => {
+//     const response = await request(app).get("/users");
+//     expect(response.status).toBe(200);
+//     expect(response.body).toEqual([
+//       {
+//         id: 1,
+//         name: "John Doe",
+//         email: "john@gmail.com",
+//         id: 2,
+//         name: "Ike Doe",
+//         email: "ike@gmail.com",
+//       },
+//     ]);
+//   });
+// });
